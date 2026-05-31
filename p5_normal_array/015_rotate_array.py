fd22c451989f2e5954259930ba967b72eacb1d96
Gilililila <1846888734@qq.com>
2026-05-31 23:37:17 +0800

    refactor: 修改015注释

--- p5_normal_array/015_rotate_array.py
+++ p5_normal_array/015_rotate_array.py
@@ -7,8 +7,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
 
-        先拼接两个相同的 nums 数组，此时我们注意到，所需的右旋转 k 步数组在这个大数组中，我们可以通过切片得到。
-        由于我们是从左向右开始切片，等价于左旋转这个数组，也就是左旋转 len(nums)-k 步
+        先拼接两个相同的 nums 数组，此时我们注意到，所需的右轮转 k 步数组在这个大数组中，我们可以通过切片得到。
+        由于我们是从左向右开始切片，等价于左轮转这个数组，也就是左轮转 len(nums)-k 步
         """
         l = len(nums)
         add_nums = nums + nums
@@ -18,8 +18,8 @@ class Solution:
 
     def rotate2(self, nums: List[int], k: int) -> None:
         """
-        把一个子数组旋转两次，相当于没有变化。
-        将数组分为两块 nums = A + B; 我们需要的旋转数组也就是 nums = B + A
+        把一个子数组反转两次，相当于没有变化。
+        将数组分为两块 nums = A + B; 我们需要的轮转数组也就是 nums = B + A
         所以我们先将 nums 进行反转，得到 rev(B) + rev(A); 然后对各个部分进行反转，得到 B + A
         """
         def reverse(left: int, right: int) -> None:
