c3465cf5296cb931ac7880cc3b085faa33f74da5
Gilililila <1846888734@qq.com>
2026-06-01 00:05:38 +0800

    feat: 完成题16-除了自身以外数组的乘积

---
+++ p5_normal_array/016_product_of_array_except_self.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pre_prod = [1] * len(nums)
+        for i in range(1, len(nums)):
+            pre_prod[i] = pre_prod[i - 1] * nums[i - 1]
+
+        post_prod = [1] * len(nums)
+        for j in range(len(nums) - 2, -1, -1):
+            post_prod[j] = post_prod[j + 1] * nums[j + 1]
+
+        res = [i * j for i, j in zip(pre_prod, post_prod)]
+
+        return res
+
+test_cases = [
+    [1,2,3,4],
+    [-1,1,0,-3,3]
+]
+
+for case in test_cases:
+    s = Solution()
+    print(s.productExceptSelf(case))
