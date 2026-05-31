c572a7ff778bef4e08d706367d633c2aa0b340dd
Gilililila <1846888734@qq.com>
2026-06-01 00:24:12 +0800

    feat: 完成题16空间优化

--- p5_normal_array/016_product_of_array_except_self.py
+++ p5_normal_array/016_product_of_array_except_self.py
@@ -2,16 +2,31 @@ from typing import List
 
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre_prod = [1] * len(nums)
-        for i in range(1, len(nums)):
+    def productExceptSelf1(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        pre_prod = [1] * n
+        for i in range(1, n):
             pre_prod[i] = pre_prod[i - 1] * nums[i - 1]
 
-        post_prod = [1] * len(nums)
-        for j in range(len(nums) - 2, -1, -1):
+        post_prod = [1] * n
+        for j in range(n - 2, -1, -1):
             post_prod[j] = post_prod[j + 1] * nums[j + 1]
 
         res = [i * j for i, j in zip(pre_prod, post_prod)]
+
+        return res
+
+    def productExceptSelf2(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        for i in range(1, n):
+            res[i] = res[i - 1] * nums[i - 1]
+
+        post = 1
+        for i in range(n - 1, -1, -1):
+            res[i] *= post
+            post *= nums[i]
 
         return res
 
@@ -22,4 +37,4 @@ test_cases = [
 
 for case in test_cases:
     s = Solution()
-    print(s.productExceptSelf(case))
+    print(s.productExceptSelf2(case))
