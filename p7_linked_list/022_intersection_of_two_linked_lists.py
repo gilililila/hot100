c3a02313cfb182da8723aea4c5aef940b2c0fd6c
Gilililila <1846888734@qq.com>
2026-06-01 21:58:08 +0800

    feat: 完成题22-相交链表

---
+++ p7_linked_list/022_intersection_of_two_linked_lists.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b = headA, headB
+        while a is not b:
+            a = a.next if a else headA
+            b = b.next if b else headB
+        return a
+
+
+"""
+test_cases = [
+    [listA, listB, intersectVal]
+]
+"""
+test_cases = [
+    [[4,1,8,4,5],[5,6,1,8,4,5],8],
+    [[1,9,1,2,4],[3,2,4],2],
+    [[2,6,4],[1,5],0],
+]
